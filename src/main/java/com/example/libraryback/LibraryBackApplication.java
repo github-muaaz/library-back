7caccee90d7bb800db75ccf5f3c45dc358a5c070
Muaaz [Muhammadaziz] <muhammadaziz.erg.1229@gmail.com>
2024-02-04 17:58:45 +0500

    Initial commit

---
+++ src/main/java/com/example/libraryback/LibraryBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBackApplication.class, args);
+	}
+
+}
